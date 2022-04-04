b983abdf7633bb5e6fe6094478801a46c4dbb8e2
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-04-04 12:41:55 +0600

    json file create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Component/Header/Header';
+import Home from './Component/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        
+      </Routes>
     </div>
   );
 }
