20192647385d3edc6558d29dfe0a23c6d312376a
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-04-05 11:53:06 +0600

    first question

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -4,6 +4,8 @@ const Blogs = () => {
     return (
         <div>
             <h1 className='text-4xl'>What is Context API?</h1>
+            <p>Context api is a way of react app which pass global variables. It is a alternative way of prop drilling. Context api  using a state management it's called Redux. Context api returns provider and consumer. provider is grandParent component and consumer is child component.
+            </p>
         </div>
     );
 };
